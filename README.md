d7667cd2b7c2c0f0c7e177b21c8e2570bb2b455f
zBiggui <bigguisenna@gmail.com>
2022-11-13 14:49:06 -0300

    feat: add logic to generate an unique key name

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Starting project for a course on Advanced Node @ Udemy
 Commom problems and possible solutions:
 
 ![cache-commom-problems](./assets/cache-problems.png)
+
+Logic of Key name:
+
+![logic-of-key-representation](./assets/keyname.png)
--- services/cache.js
+++ services/cache.js
@@ -6,6 +6,14 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function () {
   console.log("IM ABOUT TO RUN A QUERY");
 
+  // Copy one object to another without modify the original object
+  // Asign 'this.getQuery' to an empty object
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+
+  console.log(key);
+
   // To execute the original method
   return exec.apply(this, arguments);
 };
