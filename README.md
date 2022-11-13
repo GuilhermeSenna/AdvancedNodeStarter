c3a595fd6d18f22ba0c03f20229304d0d4facd50
zBiggui <bigguisenna@gmail.com>
2022-11-13 20:33:16 -0300

    feat: add complete logic for caching

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Commom problems and possible solutions:
 Logic of Key name:
 
 ![logic-of-key-representation](./assets/keyname.png)
+
+Cache logic:
+
+- Save cache with specific ID (user ID), check if has ID before query the Database
+  - Need to overwrite default methods in database tool usage (mongoose -> mongoDB)
+- Toggleable database cache (.cache() in method) w/ dynamic key (options object in cache parameters)
+- Clear cache (to a specific user) after add new post
--- services/cache.js
+++ services/cache.js
@@ -1,19 +1,57 @@
 const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
 
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+  return this;
+};
+
 // Overwrite original method
-mongoose.Query.prototype.exec = function () {
-  console.log("IM ABOUT TO RUN A QUERY");
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
   // Copy one object to another without modify the original object
   // Asign 'this.getQuery' to an empty object
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
-  console.log(key);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key);
+
+  // If we do, return that
+  if (cacheValue) {
+    // Convert array/object result to mongoose model
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // Otherwise, issue the query and store the result in redis
 
   // To execute the original method
-  return exec.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
+
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
+
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
 };
